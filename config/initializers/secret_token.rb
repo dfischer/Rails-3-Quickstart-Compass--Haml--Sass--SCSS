62102023f98bbbd5c04f1e80cb3850f1173dc626
DFischer <me@danielfischer.com>
2011-04-18 18:25:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCompassHamlSassScss::Application.config.secret_token = 'fb18de4cdfb5a8d89bdd9bdc3dc37cba5dacb134a2f2369881b4582ef473265244a5f5020f91e0a2f1179054620d1ddf88e930c943944cac0ef24cd8488b98dc'
